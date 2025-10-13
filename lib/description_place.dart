5f3daacc64ddc60b00fdb6d889afb92669c0d750
Daniel M. S <181101103+apprenticeDan@users.noreply.github.com>
2025-10-13 11:26:59 -0400

    places: widgets, description_place, constructor, font.

---
+++ lib/description_place.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+
+  late String textTit;
+  late int cantEstre;
+  late String textDescr;
+
+  DescriptionPlace(this.textTit, this.cantEstre, this.textDescr);
+
+  @override
+  Widget build(BuildContext context) {
+    final titulo = Container(
+      margin: EdgeInsets.only(
+        right: 10
+      ),
+      child: Text(
+        textTit,//"Duwilli Ella",
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontSize: 34,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+
+    final estrella = Container(
+        margin: EdgeInsets.only(
+            right: 10
+        ),
+        child: Icon(
+          Icons.star,
+        color: Colors.amber,)
+
+    );
+
+    final estrellaBorde = Container(
+      margin: EdgeInsets.only(
+          right: 15
+      ),
+      child: Icon(
+          Icons.star_border,
+          color: Colors.black54
+      ),
+    );
+
+    final filaEstrellas = Row(
+      children:
+      List.generate(cantEstre, (index) {
+        return index < cantEstre-1 ? estrella : estrellaBorde;}
+      /*<Widget>[
+        estrella,
+        estrella,
+        estrella,
+        estrella,
+        estrellaBorde
+      ],*/
+    ));
+
+    final filaTitulo = Row(
+      children: <Widget>[
+        titulo,
+        filaEstrellas
+      ],
+    );
+
+    final descripcion = Container(
+        margin: EdgeInsets.only(
+          top: 10
+        ),
+        child: Text(
+            textDescr,
+            style: TextStyle(
+              fontFamily: "Lato",
+            color: Colors.indigo,
+        ),
+        ),
+    );
+    final descriptionPlace = Column(
+      children: <Widget>[
+        filaTitulo,
+        descripcion,
+      ],
+    );
+    return descriptionPlace;
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+import 'home.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Places",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: MyHome(),
+    );
+  }
+}
